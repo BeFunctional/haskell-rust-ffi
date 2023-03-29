d94cc0bd44da38aff66feeec5deae890b0c04f4f
Edsko de Vries <edsko@well-typed.com>
2023-03-29 13:39:27 +0200

    Simplify the external API

--- haskell-ffi/src/to_haskell.rs
+++ haskell-ffi/src/to_haskell.rs
@@ -179,13 +179,9 @@ pub extern "C" fn haskell_ffi_external_len(vec: *mut Vec<u8>) -> usize {
 
 /// Free the vector
 ///
-/// The second argument is a pointer to the data held by the vector (see
-/// `haskell_ffi_external_ptr`). It is not actually used by this function; it is
-/// an argument only to match the Haskell @FinalizerEnvPtr@ type.
-///
 /// User code should not normally need to call this directly
 /// (it is called by the Haskell function @fromExternalBorsh@).
 #[no_mangle]
-pub extern "C" fn haskell_ffi_external_free(vec: *mut Vec<u8>, _ptr: *const u8) {
+pub extern "C" fn haskell_ffi_external_free(vec: *mut Vec<u8>) {
     let _vec = unsafe { Box::from_raw(vec) };
 }
